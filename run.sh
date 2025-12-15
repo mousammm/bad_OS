7d0b605909d07efa33d74ddc193062b98e05977e
mousammm <mxmssdia@gmail.com>
2025-12-15 12:36:27 +0530

    vnc set to :1

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash
 
-nasm -f bin boot.asm -o bin/boot.bin
-qemu-system-x86_64 -drive format=raw,file=bin/boot.bin &
+nasm -f bin src/boot.asm -o bin/boot.bin
+qemu-system-x86_64 -drive format=raw,file=bin/boot.bin -vnc :1 &
 sleep 2
 vncviewer localhost:5901
 wait
